34db717962d5736ceec0f2ebad59a3b9fb86e9b7
leeSH <118698639+sbturtle@users.noreply.github.com>
2024-11-04 16:46:19 +0900

    [level 2] Title: 구명보트, Time: 1.63 ms, Memory: 4.73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> p, int limit) {
+    int answer = 0;
+    sort(p.begin(), p.end());
+    int st = 0, ed = p.size() - 1;
+    
+    while(st < ed) {                   
+        if(p[st] + p[ed] <= limit)
+            st++;
+        
+        ed--;        
+        answer++;
+    }
+    if(st == ed)
+        answer++;
+    
+    return answer;
+}
