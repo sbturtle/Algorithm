99fe8ed54363bf9934c6ebe9745d0aac25adf9c8
leeSH <118698639+sbturtle@users.noreply.github.com>
2024-11-04 10:52:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41476 KB -BaekjoonHub

--- C++17/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ C++17/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,31 +1,40 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int w[101];
-int v[101];
+#define W first
+#define V second
+int n, k;
+
 int dp[101][100001];
+
 int main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    int n, k;
-    cin >> n >> k;
+	cin >> n >> k;
+	vector<pair<int, int>> p(n + 1);
 
-    for (int i = 1; i <= n; i++) {
-        cin >> w[i];
-        cin >> v[i];
-    }
+	int result = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		int w, v;
+		cin >> w >> v;
+		p[i] = { w,v };
+	}
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= k; j++) {
-            if (j >= w[i])
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-            else
-                dp[i][j] = dp[i - 1][j];
-        }
-    }
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j >= p[i].W)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - p[i].W] + p[i].V);
+			else
+				dp[i][j] = dp[i - 1][j];
+		}
+	}
 
-    cout << dp[n][k];
 
+	cout << dp[n][k];
+
+
+	return 0;
 }
