7ad163f545e1211f26c01ad74d9c14456e4356bd
leeSH <118698639+sbturtle@users.noreply.github.com>
2025-09-08 22:15:48 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 660 ms, Memory: 74140 KB -BaekjoonHub

---
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] dx = new int[] { 1, 0, -1, 0 };
+	static int[] dy = new int[] { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[n][m];
+		int[][] isvis = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			char[] ch = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = ch[j] - '0';
+			}
+			Arrays.fill(isvis[i], -1);
+		}
+		List<Integer> list = new ArrayList<>();
+
+		Queue<int[]> q = new ArrayDeque<>();
+		// 벽을 기준으로 갈 수 있는 칸의 수를 출력하기
+		// 모든 벽에 대해 해야 하기에, 그냥 0 덩어리를 먼저 찾아, 인덱스 번호를 부여하고
+		// 각 인덱스의 크기를 제공하는 것은 어떨까?
+		// 그럼 각 벽별로 인접한 0의 인덱스들의 합 + 자기 자신의 크기 1로 끝날 것같은데.
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (arr[i][j] == 1 || isvis[i][j] != -1)
+					continue;
+				int cnt = 1;
+				isvis[i][j] = list.size();
+				q.offer(new int[] { i, j });
+
+				while (!q.isEmpty()) {
+					int[] cur = q.poll();
+					for (int d = 0; d < 4; d++) {
+						int nx = dx[d] + cur[0];
+						int ny = dy[d] + cur[1];
+						if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+							continue;
+						if (arr[nx][ny] == 1 || isvis[nx][ny] != -1)
+							continue;
+						isvis[nx][ny] = isvis[cur[0]][cur[1]];
+						cnt++;
+						q.offer(new int[] { nx, ny });
+					}
+				}
+				list.add(cnt);
+			}
+		}
+
+		int[][] result = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (arr[i][j] == 0)
+					continue;
+				int cnt = 1;
+				int[] f = new int[] { -1, -1, -1, -1 };
+				outer: for (int d = 0; d < 4; d++) {
+					int nx = dx[d] + i;
+					int ny = dy[d] + j;
+					if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+						continue;
+					if (arr[nx][ny] == 1)
+						continue;
+					for (int k = 0; k < d; k++) {
+						if (isvis[nx][ny] == f[k])
+							continue outer;
+					}
+					f[d] = isvis[nx][ny];
+					// 같은 곳에 방문은 할 수 없음
+					cnt += list.get(isvis[nx][ny]);
+				}
+
+				result[i][j] = cnt % 10;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				sb.append(result[i][j]);
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+	}
+
+}
